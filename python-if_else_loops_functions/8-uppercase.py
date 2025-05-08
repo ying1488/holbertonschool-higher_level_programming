d65d45025ea0bd12498b5969051054b4dfd19fff
ying1488 <yingintech@gmail.com  git config --global user.name Ying>
2025-05-08 13:11:39 +0000

    uppercase

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 def uppercase(str):
+    uppercase_char = ""
     for char in str:
         if 'a' <= char <= 'z':
-            uppercase_char = char(ord(char) - 32)
+            uppercase_char += "{}".format(char(ord(char) - 32))
         else:
-            uppercase_char = char
-    print({}.format(uppercase_char, end=""))
+            uppercase_char += "{}".format(char)
+    print("{}".format(uppercase_char))

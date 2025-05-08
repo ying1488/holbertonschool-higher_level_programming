1999f3d3aa083eacc7b32f7c30b616d5c0c0a02f
ying1488 <yingintech@gmail.com  git config --global user.name Ying>
2025-05-08 01:13:01 +0000

    positive or negative

--- python-if_else_loops_functions/0-positive_or_negative.py
+++ python-if_else_loops_functions/0-positive_or_negative.py
@@ -5,4 +5,5 @@ if number < 0:
 	print("{} is negative".format(number))
 elif number == 0: 
 	print("{} is zero".format(number))
-else : print("{} is positive".format(number))
+else : 
+	print("{} is positive".format(number))

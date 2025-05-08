89792c5b3019f5aa3316961ad7cf757110843ef7
ying1488 <yingintech@gmail.com  git config --global user.name Ying>
2025-05-08 10:56:49 +0000

    is lower

--- python-if_else_loops_functions/7-islower.py
+++ python-if_else_loops_functions/7-islower.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 def islower(c):
    
-    if ord(c) > 97 and ord(c) < 123:
+    if ord(c) > 96 and ord(c) < 123:
             return True
     else:
-            return False 
+            return False
+

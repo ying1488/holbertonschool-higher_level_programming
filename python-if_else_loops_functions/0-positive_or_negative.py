9939679f9b9d5ade0542945111470f8402e17ed4
ying1488 <yingintech@gmail.com  git config --global user.name Ying>
2025-05-08 00:49:55 +0000

    positive or negative

--- python-if_else_loops_functions/0-positive_or_negative.py
+++ python-if_else_loops_functions/0-positive_or_negative.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10, 10)
-if number > 0:
-	print('positive')
+if number < 0:
+	print('negative')
 elif number == 0: 
 	print('zero')
-else : print('negative')
+else : print('positive')

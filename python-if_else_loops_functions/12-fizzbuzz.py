84eb59a87215cd85f9c3fa8484626a564246a2d9
ying1488 <yingintech@gmail.com  git config --global user.name Ying>
2025-05-08 13:51:34 +0000

    fizz buzz

--- python-if_else_loops_functions/12-fizzbuzz.py
+++ python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def fizzbuzz():
     for x in range(1, 101):
-        if((x % 3 == 0) and (x % 5 == 0)):
+        if ((x % 3 == 0) and (x % 5 == 0)):
             print("FizzBuzz", end=" ")
         elif x % 3 == 0:
             print("Fizz", end=" ")

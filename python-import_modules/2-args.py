26d7475d392084707bf5f865d521d6534c5f7205
ying1488 <yingintech@gmail.com  git config --global user.name Ying>
2025-05-11 11:29:57 +0000

    args

---
+++ python-import_modules/2-args.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+import sys
+
+if __name__ == "__main__":
+    args_num = len(sys.argv) - 1
+    if (args_num == 0):
+            print("0 arguments.")
+    elif(args_num == 1):
+        print("1 argument:")
+    else:
+        print(f"{args_num}arguments:")
+    for i in range(1, len(sys.argv)):
+         print(f"{i}: {sys.argv[i]}")

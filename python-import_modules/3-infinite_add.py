b9ca273cb7bf4e526054929b4dd67b65bc656322
ying1488 <yingintech@gmail.com  git config --global user.name Ying>
2025-05-11 11:40:59 +0000

    infinite addition

--- python-import_modules/3-infinite_add.py
+++ python-import_modules/3-infinite_add.py
@@ -1,12 +1,9 @@
 #!/usr/bin/python3
 
 import sys
-
-add = 0.0
-
-n = len(sys.argv)
-
-for i in range (1,n):
-    add += float(sys.argv[i])
-
-print(add)
+if __name__ == "__main__":
+    add = 0.0
+    n = len(sys.argv)
+    for i in range (1,n):
+        add += float(sys.argv[i])
+    print(add)

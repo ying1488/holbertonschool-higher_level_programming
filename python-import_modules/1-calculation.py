63d114524f165a5ef29f479e255ea7d4c24fbc75
ying1488 <yingintech@gmail.com  git config --global user.name Ying>
2025-05-11 02:11:30 +0000

    calculator

---
+++ python-import_modules/1-calculation.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+from calculator_1 import add, sub, mul, div
+a = 10
+b = 5
+
+if __name__ == "__main__":
+    print("{} + {} = {}".format(a, b, add(a,b)))
+    print("{} - {} = {}".format(a, b, add(a,b)))
+    print("{} * {} = {}".format(a, b, add(a,b)))
+    print("{} / {} = {}".format(a, b, add(a,b)))

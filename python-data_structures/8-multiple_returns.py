ae1aa93a52c99b12787f667dfa6e157fbff528aa
ying1488 <yingintech@gmail.com  git config --global user.name Ying>
2025-05-13 13:16:22 +0000

    task 8

--- python-data_structures/8-multiple_returns.py
+++ python-data_structures/8-multiple_returns.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
-    while sentence:
-        return(len(sentence),sentence[0])
+    if sentence:
+        return (len(sentence), sentence[0])
     else:
-        None
-
+        return (0, None)

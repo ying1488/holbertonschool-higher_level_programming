142f031113ca0182692308130fd648d4aaa613b3
ying1488 <yingintech@gmail.com  git config --global user.name Ying>
2025-05-13 04:55:39 +0000

    task 2

--- python-import_modules/tmp/4-hidden_discovery.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-
-import sys
-import hidden_4
-if __name__ == "__main__":
-    name = dir(hidden_4)
-    for name in sorted(names):
-        if not name.startswith("__"):
-            print(name)

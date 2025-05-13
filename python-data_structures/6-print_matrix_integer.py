69a1746ddb26e9958a6784fbafe7315b04a4dae3
ying1488 <yingintech@gmail.com  git config --global user.name Ying>
2025-05-13 06:40:42 +0000

    task 6

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -3,6 +3,6 @@ def print_matrix_integer(matrix=[[]]):
     for row in (matrix):
         for i, col in enumerate(row):
             if i != 0:
-                print(' ', end="")
-            print(col, end="")
+                print(" ", end="")
+            print("{:d}".format(col), end="")
         print()        

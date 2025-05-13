0714de1a4c74bffc7111c03e8ce0288630bb0984
ying1488 <yingintech@gmail.com  git config --global user.name Ying>
2025-05-13 02:21:07 +0000

    task 0

--- python-data_structures/0-print_list_integer.py
+++ python-data_structures/0-print_list_integer.py
@@ -1,5 +1,10 @@
 #!/usr/bin/python3
+# def print_list_integer(my_list=[]):
+#     list_Length = len(my_list)
+#     for x in range(list_Length):
+#         print("{:d}".format(my_list[x]))
+
+
 def print_list_integer(my_list=[]):
-    list_Length = len(my_list)
-    for x in range(list_Length):
-        print("{:d}".format(my_list[x]))
+    for x in my_list:
+        print("{:d}".format(x))

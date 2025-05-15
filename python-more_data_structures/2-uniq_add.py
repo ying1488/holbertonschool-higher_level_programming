e6c4f0a8d3bc3e7a6de645a29f7fa5059aa440a0
ying1488 <yingintech@gmail.com  git config --global user.name Ying>
2025-05-15 01:07:04 +0000

    task 2

--- python-more_data_structures/2-uniq_add.py
+++ python-more_data_structures/2-uniq_add.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
-uniq_add = __import__('2-uniq_add').uniq_add
-
-my_list = [1, 2, 3, 1, 4, 2, 5]
-result = uniq_add(my_list)
-print("Result: {:d}".format(result))
+def uniq_add(my_list=[]):
+    newList = set(my_list)
+    return sum(newList)

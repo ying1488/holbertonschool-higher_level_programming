18137da73aad8b090e530197139808855345c3a2
ying1488 <yingintech@gmail.com  git config --global user.name Ying>
2025-05-17 05:37:56 +0000

    task 7

--- python-more_data_structures/7-update_dictionary.py
+++ python-more_data_structures/7-update_dictionary.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
-def print_sorted_dictionary(a_dictionary):
-    for key in sorted(a_dictionary.keys()):
-        print(f"{key}:{a_dictionary}")
+def update_dictionary(a_dictionary, key, value):
+    a_dictionary[key] = value
+    return a_dictionary

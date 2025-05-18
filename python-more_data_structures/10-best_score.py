d4569adcc841f965cd2315168abe972dd8047d87
ying1488 <yingintech@gmail.com  git config --global user.name Ying>
2025-05-18 01:54:32 +0000

    task 10

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -1,1 +1,8 @@
 #!/usr/bin/python3
+def best_score(a_dictionary):
+    if not a_dictionary:
+        return None
+    
+    elif a_dictionary:
+        best_score = max(a_dictionary, key=a_dictionary.get)
+        return best_score

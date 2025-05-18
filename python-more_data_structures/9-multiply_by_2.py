db67ce8db60e16cf1e6ffb7f56f20b0d57e8ae0b
ying1488 <yingintech@gmail.com  git config --global user.name Ying>
2025-05-18 01:41:05 +0000

    task 9

--- python-more_data_structures/9-multiply_by_2.py
+++ python-more_data_structures/9-multiply_by_2.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 def multiply_by_2(a_dictionary):
-    mul = 2
+    new_dict = {}
     for key in a_dictionary:
-        a_dictionary[key] = a_dictionary.get(key) * mul
-    return a_dictionary
+        res = a_dictionary[key] * 2
+        new_dict[key] = res
+    return new_dict

2cdc04aa98320f1bd3447860fdff4b8fbca9a89e
ying1488 <yingintech@gmail.com  git config --global user.name Ying>
2025-05-23 12:33:01 +0000

    task 3

--- python-classes/3-square.py
+++ python-classes/3-square.py
@@ -7,7 +7,8 @@ Write a class Square that defines a square by
 
 class Square:
 
-    """A square class with size as a private attribute & public instance of area"""
+    """A square class with size as a private attribute """
+    """ & public instance of area""""
     __size = 3
 
     def __init__(self, size=0):
@@ -16,6 +17,6 @@ class Square:
         if size < 0:
             raise ValueError("size must be >= 0")
         self.__size = size
-    
+
     def area(self):
         return self.__size ** 2

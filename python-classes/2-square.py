6a826ee314ad3819c147e4119aa206beaf47fa2b
ying1488 <yingintech@gmail.com  git config --global user.name Ying>
2025-05-23 12:18:47 +0000

    task 2

---
+++ python-classes/2-square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+#!/usr/bin/python3
+"""
+Write a class Square that defines a square by
+
+"""
+
+class Square:
+    """A square classwith size as a private attribute"""
+    __size = 3
+
+    def __init__(self, size=0):
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size

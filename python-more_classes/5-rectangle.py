10bbb24ae014e37ff7b5ff7f913b5220017d975d
ying1488 <yingintech@gmail.com  git config --global user.name Ying>
2025-05-24 06:27:37 +0000

    task 5

--- python-more_classes/5-rectangle.py
+++ python-more_classes/5-rectangle.py
@@ -76,4 +76,4 @@ class Rectangle:
         return "Rectangle({}, {})".format(self.width, self.height)
 
     def __del__(self):
-        return("Bye rectangle...")
+        print("Bye rectangle...")

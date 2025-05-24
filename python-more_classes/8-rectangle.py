33dd26707e1afaafc967e94b78005e900e5a121d
ying1488 <yingintech@gmail.com  git config --global user.name Ying>
2025-05-24 07:53:23 +0000

    task 9

--- python-more_classes/8-rectangle.py
+++ python-more_classes/8-rectangle.py
@@ -96,3 +96,7 @@ class Rectangle:
         if rect_1.area() >= rect_2.area():
             return rect_1
         return rect_2
+
+    @classmethod
+    def square(cls, size=0):
+        return cls(size, size)

32ef31ffaf0d016af69690c4cc5ce5191d825d08
ying1488 <yingintech@gmail.com  git config --global user.name Ying>
2025-05-26 12:41:16 +0000

    task1

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -4,6 +4,8 @@ a class MyList that inherits from list:
 """
 
 class MyList(list):
-
+    """
+    prints the list, but sorted (ascending sort)
+    """
     def print_sorted(self):
         print(sorted(self))

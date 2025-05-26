61d680e2a29017866ae7f6e86dd6181eb20026b0
ying1488 <yingintech@gmail.com  git config --global user.name Ying>
2025-05-26 12:14:19 +0000

    task 0

--- python-inheritance/0-lookup.py
+++ python-inheritance/0-lookup.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
 """
-    Returns the list of available 
+    Returns the list of available
     attributes and methods of an object.
     """
 
+
 def lookup(obj):
     """
-    Returns the list of available 
+    Returns the list of available
     attributes and methods of an object.
     """
-    return dir (obj)
+    return dir(obj)

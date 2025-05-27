e2942bf9812de93d5e3e8d106a0191c73642900b
ying1488 <yingintech@gmail.com  git config --global user.name Ying>
2025-05-27 06:36:18 +0000

    task 9

--- python-inheritance/9-rectangle.py
+++ python-inheritance/9-rectangle.py
@@ -15,7 +15,7 @@ class Rectangle(BaseGeometry):
 
     def area(self):
         """area"""
-        return self.width * self.height
+        return self.__width * self.__height
 
     def __str__(self):
         """prints breakdown of area"""

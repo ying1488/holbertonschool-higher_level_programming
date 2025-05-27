bea1458ae2ad775948e7dc1bfda653a62b67963d
ying1488 <yingintech@gmail.com  git config --global user.name Ying>
2025-05-27 04:12:27 +0000

    task 4

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -18,5 +18,6 @@ def inherits_from(obj, a_class):
     """
     return(
         isinstance(obj, a_class)
-        and type(obj) is not a_class
+        and type(obj)
+        is not a_class
         )

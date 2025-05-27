5affc55216f0a7338413e23bdcd13828179609f7
ying1488 <yingintech@gmail.com  git config --global user.name Ying>
2025-05-27 06:35:26 +0000

    task 9

---
+++ python-inheritance/9-rectangle.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""Rectangle"""
+
+
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """Rectangle"""
+    def __init__(self, width, height):
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+        self.__width = width
+        self.__height = height
+
+    def area(self):
+        """area"""
+        return self.width * self.height
+
+    def __str__(self):
+        """prints breakdown of area"""
+        return "[Rectangle] {}/{}".format(self.__width, self.__height)

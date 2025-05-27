fd8dd9516d6b3284b85b2d5be2df511b9a3a3e3a
ying1488 <yingintech@gmail.com  git config --global user.name Ying>
2025-05-27 04:33:48 +0000

    task 4

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -5,4 +5,7 @@
 def inherits_from(obj, a_class):
     """True if the object is an instance of a class that inherited"""
 
-    return (isinstance(obj, a_class) and type(obj) is not a_class)
+    return (
+        isinstance(obj, a_class)
+        and type(obj)
+        is not a_class)

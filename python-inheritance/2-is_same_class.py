d368fc8820048d0f4f0fd9b131ba60cc445538a0
ying1488 <yingintech@gmail.com  git config --global user.name Ying>
2025-05-27 01:54:21 +0000

    task 2

--- python-inheritance/2-is_same_class.py
+++ python-inheritance/2-is_same_class.py
@@ -7,4 +7,10 @@ otherwise False.
 """
 
 def is_same_class(obj, a_class):
+    """
+    function that returns
+    True if the object is exactly
+    an instance of the specified class;
+    otherwise False.
+    """
     return type(obj) is a_class

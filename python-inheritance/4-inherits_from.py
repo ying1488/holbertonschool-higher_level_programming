b08ea244885bab83d7b2f89a3a88cd34376b9003
ying1488 <yingintech@gmail.com  git config --global user.name Ying>
2025-05-27 04:16:36 +0000

    task 4

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
 """
 Write a function that returns
-True if the object is an instance of
-a class that inherited (directly or indirectly)
-from the specified class
-otherwise False.
+True if the object is an instance
+of a class that inherited (directly or indirectly)
+from the specified class ; otherwise False.
 """
 
 
@@ -16,8 +15,5 @@ def inherits_from(obj, a_class):
     from the specified class
     otherwise False.
     """
-    return(
-        isinstance(obj, a_class)
-        and type(obj)
-        is not a_class
-        )
+    
+    return (isinstance(obj, a_class) and type(obj) is not a_class)

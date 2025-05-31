a5de36408d437093f4218cd3ed3255da53e8dd50
ying1488 <yingintech@gmail.com  git config --global user.name Ying>
2025-05-31 13:22:12 +0000

    task 1

--- python-abc/task_01_duck_typing.py
+++ python-abc/task_01_duck_typing.py
@@ -46,5 +46,5 @@ def shape_info(shape):
     shape info - prints area and perimeter of a shape
     args: shape
     """
-    print(f"area: {shape.area():.1f}")
+    print(f"Area: {shape.area():.1f}")
     print(f"Perimeter: {shape.perimeter():.1f}")

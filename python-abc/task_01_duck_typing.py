12c60c04ec2d8288673648de07609409d8270683
ying1488 <yingintech@gmail.com  git config --global user.name Ying>
2025-05-31 13:11:36 +0000

    task 1

--- python-abc/task_01_duck_typing.py
+++ python-abc/task_01_duck_typing.py
@@ -21,7 +21,7 @@ class Circle(Shape):
         self.radius = radius
 
     def area(self):
-        return self.radius**2*3.14
+        return self.radius*3.14
 
     def perimeter(self):
         return 2*self.radius*3.14

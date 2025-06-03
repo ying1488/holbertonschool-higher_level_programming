0f0a39a28507788c7931e849366221044b29d15d
ying1488 <yingintech@gmail.com  git config --global user.name Ying>
2025-06-03 06:39:26 +0000

    task 4

--- python-input_output/4-from_json_string.py
+++ python-input_output/4-from_json_string.py
@@ -6,6 +6,6 @@ import json
 
 
 def from_json_string(my_str):
-    """ a function that returns an object 
+    """ a function that returns an object
     (Python data structure) represented by a JSON string """
     return json.loads(my_str)

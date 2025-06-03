ac5f836779850480127623c8fadd7a01e2763c50
ying1488 <yingintech@gmail.com  git config --global user.name Ying>
2025-06-03 04:48:40 +0000

    task 2

--- python-input_output/2-append_write.py
+++ python-input_output/2-append_write.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python3
 """Append to a file"""
 
+
 def append_write(filename="", text=""):
     """ Append to a file
     Returns the number of characters added
     Args: file name (string) and text(string)
     """
-    with open(filename, "w", encoding="utf-8") as f:
+
+    with open(filename, "a", encoding="utf-8") as f:
         return f.write(text)

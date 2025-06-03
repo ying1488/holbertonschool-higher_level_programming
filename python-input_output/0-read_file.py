adb5028d6e234ef883b8cef8a65bdf137ae5be64
ying1488 <yingintech@gmail.com  git config --global user.name Ying>
2025-06-03 02:40:11 +0000

    task1

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
 def read_file(filename=""):
+   """read file"""
    with open("my_file_0.txt", encoding="utf-8") as f:
       print(f.read(), end="")
--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
 def write_file(filename="", text=""):
+    """write file"""
     with open("my_first_file.txt", "w", encoding="utf-8") as f:
         return f.write("text")

b0539e2eb5bc9f9adaeef86258d004d80566d13c
ying1488 <yingintech@gmail.com  git config --global user.name Ying>
2025-06-03 03:00:42 +0000

    task 1

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -4,5 +4,5 @@
 
 def write_file(filename="", text=""):
     """write file"""
-    with open("my_first_file.txt", "w", encoding="utf-8") as f:
+    with open(filename, "w", encoding="utf-8") as f:
         return f.write("text")

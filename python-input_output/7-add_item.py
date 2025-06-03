41fbf02fee9d8f2fba6af5ae80e62d1760d4b4bf
ying1488 <yingintech@gmail.com  git config --global user.name Ying>
2025-06-03 07:26:11 +0000

    task 7

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -7,9 +7,15 @@ save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
 args = sys.argv
+
 filename = "add_item.json"
-with open(filename, 'a+', encoding="utf-8") as f:
-    my_list = args[1:]
-    my_list.extend(args[1:])
-    save_to_json_file(my_list, filename)
-    load_from_json_file(filename)
+try:
+    with open(filename, 'a+', encoding="utf-8") as f:
+        items = json.load(f)
+except FileNotFoundError:
+    items = []
+
+items.extend(args[1:])
+
+with open(filename, "w", encoding="utf-8") as f:
+    json.dump(items, f)

d2db4b44525921146f9587c2ee3a97f30c1fab6b
ying1488 <yingintech@gmail.com  git config --global user.name Ying>
2025-06-04 11:15:44 +0000

    task 7

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,10 +1,14 @@
 #!/usr/bin/python3
-"""Load, add, save"""
+"""
+Load, add, save
+
+"""
+
+import sys
+import json
 
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
-import sys
-import json
 
 args = sys.argv
 

2c3f8ebcbed186994cbcd68d95c33b9cf92546ee
ying1488 <yingintech@gmail.com  git config --global user.name Ying>
2025-06-05 06:48:50 +0000

    task 9

---
+++ python-input_output/9-student.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""Student to JSON
+"""
+
+
+class Student:
+
+    def __init__(self, first_name, last_name, age): 
+        """Student
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        """retreives a dict representation 
+        of a Student instance
+        """
+        return self.__dict__

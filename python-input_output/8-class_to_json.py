9fe4abdbdc7cd2cc7292ff92f7c5512d238cf1aa
ying1488 <yingintech@gmail.com  git config --global user.name Ying>
2025-06-05 06:35:05 +0000

    task 8

--- python-input_output/8-class_to_json.py
+++ python-input_output/8-class_to_json.py
@@ -1,13 +1,12 @@
 #!/usr/bin/python3
 
 """ Class to JSON
-    a function that returns the dictionary
-    description with simple data structure
-    of an object
 """
-
-import json
 
 
 def class_to_json(obj):
+    """ a function that returns the dictionary
+    description with simple data structure
+    of an object
+    """
     return obj.__dict__

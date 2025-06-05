f2e5ddd91d6978b39755e70c4e1895d04b7e6fa3
ying1488 <yingintech@gmail.com  git config --global user.name Ying>
2025-06-05 06:32:19 +0000

    task 8

---
+++ python-input_output/8-class_to_json.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+""" Class to JSON
+    a function that returns the dictionary
+    description with simple data structure
+    of an object
+"""
+
+import json
+
+
+def class_to_json(obj):
+    return obj.__dict__
--- python-input_output/8-my_class.py
+++ python-input_output/8-my_class.py
@@ -1,3 +1,13 @@
-__str__
+""" My class module
+"""
 
-class object to something printable
+class MyClass:
+    """ My class
+    """
+
+    def __init__(self, name):
+        self.name = name
+        self.number = 0
+
+    def __str__(self):
+        return "[MyClass] {} - {:d}".format(self.name, self.number)

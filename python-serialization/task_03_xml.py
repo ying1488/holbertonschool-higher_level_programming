47db5326504acfc765a554d2f226b511fb10e22f
ying1488 <yingintech@gmail.com  git config --global user.name Ying>
2025-06-08 10:24:24 +0000

    task 3

---
+++ python-serialization/task_03_xml.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""Serializing and Deserializing with XML"""
+
+
+import json
+import xml.etree.ElementTree as ET
+
+
+def serialize_to_xml(dictionary, filename):
+    """take a Python dictionary
+    and a filename as parameters.
+    serialize the dictionary into XML
+    and save it to the given filename."""
+    root = ET.Element("root")
+    for k in dictionary.keys():
+        ET.SubElement(root, k).text = dictionary[k]
+
+    tree = ET.ElementTree(root)
+    tree.write(filename)
+
+
+def deserialize_from_xml(filename):
+    """This will take a filename as its parameter,
+    read the XML data from that file,
+    and return a deserialized Python dictionary."""
+    tree = ET.parse(filename)
+    root = tree.getroot()
+    data = {}
+    for child in root:
+        data[child.tag] = child.text
+
+    return data

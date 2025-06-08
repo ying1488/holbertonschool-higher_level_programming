81ae76d4bdb5b66223e6e4d73c507775b4e4927c
ying1488 <yingintech@gmail.com  git config --global user.name Ying>
2025-06-08 05:31:35 +0000

    task 00

---
+++ python-serialization/task_00_basic_serialization.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""Basic Serialization
+"""
+
+import json
+
+
+def serialize_and_save_to_file(data, filename):
+    # Your code here to serialize and save data to the specified file
+    json_data = json.dumps(data, filename)
+    print(json_data)
+    pass
+
+
+def load_and_deserialize(filename):
+    # Your code here to load and deserialize data from the specified file
+    des_data = filename(**json.loads(filename))
+    print(des_data)
+    pass

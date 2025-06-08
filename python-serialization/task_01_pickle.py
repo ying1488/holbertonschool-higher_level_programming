93782b4e5b86cb6501264f309444475fc3ebcb73
ying1488 <yingintech@gmail.com  git config --global user.name Ying>
2025-06-08 09:50:29 +0000

    task 1

--- python-serialization/task_01_pickle.py
+++ python-serialization/task_01_pickle.py
@@ -4,26 +4,26 @@
 
 import pickle
 
+
 class CustomObject:
     def __init__(self, name, age, is_student):
         self.name = name
         self.age = age
         self.is_student = is_student
-    
+
     def display(self):
-        print(f"Name: {self.name}, Age: {self.age}, Student: {self.is_student}")
+        print(f"Name: {self.name},Age: {self.age}, Student: {self.is_student}")
 
     def serialize(self, filename):
-        with open(filename, 'w') as f:
+        with open(filename, 'wb') as f:
             pickle.dump(self, f)
 
     @classmethod
     def deserialize(cls, filename):
         try:
-            with open (filename, 'rb') as f:
+            with open(filename, 'rb') as f:
                 data = pickle.load(f)
                 return (data)
-        except(EOFError, pickle.UnpicklingError):
+        except (EOFError, pickle.UnpicklingError):
             print("file is damaged")
         return
-

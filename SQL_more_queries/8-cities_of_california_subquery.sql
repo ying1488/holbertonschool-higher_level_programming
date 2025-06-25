5afbd771fb567fcaa703416b299964dcea4d096f
ying1488 <yingintech@gmail.com  git config --global user.name Ying>
2025-06-25 13:02:27 +0000

    task 8

---
+++ SQL_more_queries/8-cities_of_california_subquery.sql
@@ -0,0 +1,4 @@
+-- a script that lists all the cities of California 
+SELECT cities.id, cities.name FROM cities
+WHERE state_id = (SELECT id FROM states WHERE name = 'California')
+ORDER BY cities.id ASC;

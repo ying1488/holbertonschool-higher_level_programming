e4fa3f575052094b0fd857682ce1d9792ea36fb7
ying1488 <yingintech@gmail.com  git config --global user.name Ying>
2025-06-25 12:27:24 +0000

    task 6

--- SQL_more_queries/6-states.sql
+++ SQL_more_queries/6-states.sql
@@ -1,6 +1,7 @@
 -- creates the database hbtn_0d_usa and the table states
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+USE hbtn_0d_usa;
 CREATE TABLE IF NOT EXISTS states (
-    id INT NOT NULL UNIQUE, 
+    id INT PRIMARY KEY UNIQUE AUTO_INCREMENT NOT NULL, 
     name VARCHAR(256) NOT NULL
 );

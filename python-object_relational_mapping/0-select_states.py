ef4b49fb9a6de2011269572c8235de40fe02a4b1
ying1488 <yingintech@gmail.com  git config --global user.name Ying>
2025-06-29 07:52:25 +0000

    task 0

---
+++ python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+""" This module is for listing all states using MySQLdb"""
+
+if __name__ == "__main__":
+    import MySQLdb
+    import sys
+    db_connection = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=sys.argv[1],
+        passwd=sys.argv[2],
+        db=sys.argv[3]
+    )
+
+    cursor = db_connection.cursor()
+    cursor.excute("SELECT * FROM states ORDER BY id ASC")
+    states = cursor.fetchall()
+
+    for state in states:
+        print(state)
+    cursor.close()
+    db_connection.close()

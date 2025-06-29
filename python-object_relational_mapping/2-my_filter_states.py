7454fd0aba8b2896fbee90b31b20a78b5424215a
ying1488 <yingintech@gmail.com  git config --global user.name Ying>
2025-06-29 08:32:15 +0000

    task 2

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 """This module is for filter states by user inputusing MySQLdb"""
+import MySQLdb
+import sys
 
 # connecting to MySQL database
 if __name__ == "__main__":
-    import MySQLdb
-    import sys
     db_connection = MySQLdb.connect(
         host="localhost",
         port=3306,
@@ -16,14 +16,13 @@ if __name__ == "__main__":
     name_search = sys.argv[4]
     cursor = db_connection.cursor()
     # Executing MySQL Queries in Python
-    format = "SELECT * FROM states WHERE name = BINARY %s ORDER BY id ASC"
-    cursor.execute(format, (name_search))
+    q = (
+        "SELECT * FROM states WHERE BINARY" 
+        "name = '{}' ORDER BY id ASC"
+    )
+    cursor.execute(q)
     # Obtaining Query Results
     n_states = cursor.fetchall()
 
     for n in n_states:
         print(n)
-
-    # Clean Up
-    cursor.close()
-    db_connection.close()

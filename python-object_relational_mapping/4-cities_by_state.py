797f61ca18f8144fe235e141dd4e3e0fa1bd2300
ying1488 <yingintech@gmail.com  git config --global user.name Ying>
2025-06-30 13:45:07 +0000

    task 4

--- python-object_relational_mapping/4-cities_by_state.py
+++ python-object_relational_mapping/4-cities_by_state.py
@@ -18,7 +18,7 @@ if __name__ == "__main__":
     )
 
     cursor = db_connection.cursor()
-    q = "SELECT * FROM cities JOIN states ON cities.state_id = states.id "
+    q = "SELECT cities.id, cities.name, states.name FROM cities JOIN states ON cities.state_id = states.id "
     "ORDER BY cities.id ASC"
     cursor.execute(q)
 

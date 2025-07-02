d5df08544bc382d7dbf1fa17d4db203090e6f825
ying1488 <yingintech@gmail.com  git config --global user.name Ying>
2025-07-02 01:22:00 +0000

    task 4

--- python-object_relational_mapping/5-filter_cities.py
+++ python-object_relational_mapping/5-filter_cities.py
@@ -20,8 +20,9 @@ if __name__ == "__main__":
 
     cursor = db_connection.cursor()
     q = """
-        SELECT cities.name FROM cities JOIN states 
-        ON cities.state_id = states.id WHERE states.name = %s
+        SELECT cities.name FROM cities 
+        JOIN states ON cities.state_id = states.id 
+        WHERE states.name = %s
         ORDER BY cities.id ASC
         """
     cursor.execute(q, (state_name,))

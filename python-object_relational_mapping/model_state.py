b91257ca60cfea820e4fe17c853002b75067efb0
ying1488 <yingintech@gmail.com  git config --global user.name Ying>
2025-07-02 05:54:23 +0000

    task 6

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -6,5 +6,5 @@ Base = declarative_base()
 class State(Base):
     __tablename__ = 'State'
 
-    id = Column(Integer, primary_key=True)
-    name = Column(String)
+    id = Column(Integer, autoincrement=True, nullable=False, primary_key=True)
+    name = Column(String(128), nullable=False)

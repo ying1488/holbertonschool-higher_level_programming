8a5563900e879e627576dec26c9c98f45c66f5c0
ying1488 <yingintech@gmail.com  git config --global user.name Ying>
2025-07-02 06:14:24 +0000

    task 7

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -2,7 +2,6 @@
 """First state model"""
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, Integer, String
-from sqlalchemy.orm import relationship
 
 Base = declarative_base()
 
@@ -11,7 +10,7 @@ class State(Base):
     """class"""
     __tablename__ = 'states'
 
-    id = Column(Integer, primary_key=True, unique=True,
+    id = Column(Integer, primary_key=True,
                 autoincrement=True, nullable=False)
     name = Column(String(128), nullable=False)
-    cities = relationship('City', back_populates='state')
+    

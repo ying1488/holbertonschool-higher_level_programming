62865fb90927707b9ef4826db9bb5055df83cbe0
ying1488 <yingintech@gmail.com  git config --global user.name Ying>
2025-07-03 11:47:00 +0000

    task 7

--- python-object_relational_mapping/7-model_state_fetch_all.py
+++ python-object_relational_mapping/7-model_state_fetch_all.py
@@ -18,7 +18,10 @@ if __name__ == "__main__":
 
     Session = sessionmaker(bind=engine)
     session = Session()
+    Base.metadata.create_all(engine)
 
     states = session.query(State).order_by(State.id).all()
     for state in states:
         print("{}: {}".format(state.id, state.name))
+
+    session.close()

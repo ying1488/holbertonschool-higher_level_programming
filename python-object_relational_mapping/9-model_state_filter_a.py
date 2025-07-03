6f4b872ed1d0e22c883c59f994fd420c30accd7f
ying1488 <yingintech@gmail.com  git config --global user.name Ying>
2025-07-03 12:52:44 +0000

    task 9

--- python-object_relational_mapping/9-model_state_filter_a.py
+++ python-object_relational_mapping/9-model_state_filter_a.py
@@ -6,7 +6,6 @@ from sqlalchemy.orm import sessionmaker
 from model_state import Base, State
 
 if __name__ == "__main__":
-
     username = sys.argv[1]
     password = sys.argv[2]
     db = sys.argv[3]
@@ -15,13 +14,15 @@ if __name__ == "__main__":
         'mysql+mysqldb://{}:{}@localhost:3306/{}'.
         format(username, password, db)
         )
+    Base.metadata.create_all(engine)
 
     Session = sessionmaker(bind=engine)
     session = Session()
-    Base.metadata.create_all(engine)
-
-    states = session.query(State).filter(State.name.like('%a%')).order_by(State.id).all()
-    for state in states:
-        print("f{state.id}: {state.name}")
+    
+    states_a = session.query(State)\
+        .filter(State.name.like('%a%'))\
+        .order_by(State.id).all()
+    for state in states_a:
+        print(f"{state.id}: {state.name}")
 
     session.close()

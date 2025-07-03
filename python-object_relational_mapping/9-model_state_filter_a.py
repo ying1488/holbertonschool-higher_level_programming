1ba87ec05cd8b0086bc7edd570469fa892d89d9a
ying1488 <yingintech@gmail.com  git config --global user.name Ying>
2025-07-03 12:45:46 +0000

    task 9

---
+++ python-object_relational_mapping/9-model_state_filter_a.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""Prints the first State objects from the database hbtn_0e_6_usa"""
+import sys
+from sqlalchemy import (create_engine)
+from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
+
+if __name__ == "__main__":
+
+    username = sys.argv[1]
+    password = sys.argv[2]
+    db = sys.argv[3]
+
+    engine = create_engine(
+        'mysql+mysqldb://{}:{}@localhost:3306/{}'.
+        format(username, password, db)
+        )
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    Base.metadata.create_all(engine)
+
+    states = session.query(State).filter(State.name.like('%a%')).order_by(State.id).all()
+    for state in states:
+        print("f{state.id}: {state.name}")
+
+    session.close()

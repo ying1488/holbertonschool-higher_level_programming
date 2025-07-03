2fb7c9848be4c4690f34223a3fffec5dc7a2516a
ying1488 <yingintech@gmail.com  git config --global user.name Ying>
2025-07-03 12:01:00 +0000

    task 8

--- python-object_relational_mapping/8-model_state_fetch_first.py
+++ python-object_relational_mapping/8-model_state_fetch_first.py
@@ -23,7 +23,7 @@ if __name__ == "__main__":
     states = session.query(State).order_by(State.id).first()
     if states:
         print("{}: {}".format(states.id, states.name))
-    else:    
+    else:
         print("Nothing")
 
     session.close()

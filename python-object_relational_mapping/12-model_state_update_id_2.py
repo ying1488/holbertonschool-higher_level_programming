4f07c810b74a8f988f76ebc6b0d4bba9928a7257
ying1488 <yingintech@gmail.com  git config --global user.name Ying>
2025-07-06 00:44:47 +0000

    task12

--- python-object_relational_mapping/12-model_state_update_id_2.py
+++ python-object_relational_mapping/12-model_state_update_id_2.py
@@ -4,14 +4,13 @@ with the name passed as argument from the database hbtn_0e_6_usa
 """
 from sqlalchemy.orm import sessionmaker
 import sys
-from sqlalchemy import (create_engine)
+from sqlalchemy import create_engine
 from model_state import Base, State
 
 if __name__ == "__main__":
     user = sys.argv[1]
     password = sys.argv[2]
     db_name = sys.argv[3]
-    name_search = sys.argv[4]
 
     engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
         sys.argv[1], sys.argv[2], sys.argv[3])
@@ -21,10 +20,8 @@ if __name__ == "__main__":
     session = Session()
     Base.metadata.create_all(engine)
 
-    new_state = State(name="Louisiana")
-    session.add(new_state)
-    session.commit()
-    print(new_state.id)
-
-
+    updated_state = session.query(State).filter(State.id == 2).first()
+    if updated_state:
+        updated_state.name = "New Mexico"
+        session.commit()
     session.close()

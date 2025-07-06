9ff6a227af42e00df93d5951289e17c9c4d14b7c
ying1488 <yingintech@gmail.com  git config --global user.name Ying>
2025-07-06 00:53:27 +0000

    task 14

--- python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -1,28 +1,38 @@
 #!/usr/bin/python3
-""" This module is for listing all city objects
-from the database hbtn_0e_6_usa
+"""Start link class to table in database
 """
-from sqlalchemy.orm import sessionmaker
 import sys
-from sqlalchemy import (create_engine)
 from model_state import Base, State
 from model_city import City
+from sqlalchemy.orm import Session
+
+
+from sqlalchemy import (create_engine)
 
 if __name__ == "__main__":
-    user = sys.argv[1]
+    if len(sys.argv) != 4:
+        sys.exit("Usage: ./script.py <username> <password> "
+                 "<database>")
+
+    username = sys.argv[1]
     password = sys.argv[2]
     db_name = sys.argv[3]
 
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
-        sys.argv[1], sys.argv[2], sys.argv[3])
-        )
+    engine = create_engine(
+        "mysql+mysqldb://{}:{}@localhost:3306/{}"
+        .format(username, password, db_name),
+        pool_pre_ping=True)
 
-    Session = sessionmaker(bind=engine)
-    session = Session()
     Base.metadata.create_all(engine)
 
-    cities = session.query(City).join(State).order_by(City.id).all()
-    for city in cities:
-        print(f"{city.state.name}: ({city.id}) {city.name}")
+    session = Session(engine)
+
+    # Join State and City tables and order by City.id
+    results = session.query(State.name, City.id, City.name)\
+        .join(City, City.state_id == State.id)\
+        .order_by(City.id).all()
+
+    for state_name, city_id, city_name in results:
+        print(f"{state_name}: ({city_id}) {city_name}")
 
     session.close()

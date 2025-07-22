8699fba749c3f3efc8d1001c05df5dddcc232f59
ying1488 <yingintech@gmail.com  git config --global user.name Ying>
2025-07-22 13:56:04 +0000

    task 7

--- javascript-warm_up/7-multi_c.js
+++ javascript-warm_up/7-multi_c.js
@@ -2,11 +2,10 @@
 const str = 'C is fun';
 const x = parseInt(process.argv[2]);
 
-if (!Number.isInteger(x)){
+if (!Number.isInteger(x)) {
   console.log('Missing number of occurrences');
 } else {
-  for (let i = 0; i < x; i++)
-  {
+  for (let i = 0; i < x; i++) {
     console.log(str);
   }
 }

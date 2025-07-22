0f8c5dcddb66eaee56b5516c06e8eb7c545e0ba3
ying1488 <yingintech@gmail.com  git config --global user.name Ying>
2025-07-22 04:28:11 +0000

    task 3

--- javascript-warm_up/2-arguments.js
+++ javascript-warm_up/2-arguments.js
@@ -1,8 +1,10 @@
 #!/usr/bin/node
 
 const arg = process.argv[2];
-if (arg === undefined) {
+if (process.argv.length - 2 === 0) {
     console.log('No argument');
+} else if (process.argv.length - 2  === 1){
+    console.log('Argument found');
 } else {
-    console.log(arg);
+    console.log('Argument found');
 }

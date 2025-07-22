9d27219c5b893ecb1358b768360860729190d394
ying1488 <yingintech@gmail.com  git config --global user.name Ying>
2025-07-22 13:32:53 +0000

    task 6

--- javascript-warm_up/6-multi_languages_loop.js
+++ javascript-warm_up/6-multi_languages_loop.js
@@ -1,8 +1,10 @@
 #!/usr/bin/node
 const string = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 
-for (let i = 0; i < string.length; i++)
-{
-    console.log(string[i]);
+let i = 0;
+
+while (i < string.length) {
+  console.log(string[i]);
+  i++;
 }
 

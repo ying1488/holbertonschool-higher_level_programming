6fe0e5b54e4267a5f88801562250bcfd4e644866
ying1488 <yingintech@gmail.com  git config --global user.name Ying>
2025-07-23 01:10:55 +0000

    task 9

--- javascript-warm_up/9-add.js
+++ javascript-warm_up/9-add.js
@@ -2,10 +2,9 @@
 const num1 = parseInt(process.argv[2]);
 const num2 = parseInt(process.argv[3]);
 
-
 if (!Number.isInteger(num1) || !Number.isInteger(num2)) {
   console.log('NaN');
 } else {
-  let sum = num1 + num2;
-  console.log(sum)
+  const sum = num1 + num2;
+  console.log(sum);
 }

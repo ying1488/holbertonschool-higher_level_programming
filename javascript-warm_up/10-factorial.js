85a283020115e3424fa057664cb261865267d853
ying1488 <yingintech@gmail.com  git config --global user.name Ying>
2025-07-23 01:25:38 +0000

    task 9

---
+++ javascript-warm_up/10-factorial.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+const a = parseInt(process.argv[2]);
+
+function fact (n) {
+    if (isNaN(n)) {
+        return 1;
+    }
+    let res = 1;
+    for (let i = 1; i <= n; i++) {
+        res *= i
+    }
+    return res;
+}
+console.log(fact(a));

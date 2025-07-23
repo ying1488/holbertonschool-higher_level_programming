ba9b0512a65ce8d4959f21164b186f42835ae9a0
ying1488 <yingintech@gmail.com  git config --global user.name Ying>
2025-07-23 00:55:16 +0000

    task 8

--- javascript-warm_up/8-square.js
+++ javascript-warm_up/8-square.js
@@ -7,9 +7,9 @@ if (!Number.isInteger(num)) {
 } else {
   for (let i = 0; i < num; i++) {
     let row = '';
-      for (let j = 0; j < num; j++) {
-        row += 'X'
-        }
+    for (let j = 0; j < num; j++) {
+      row += 'X'
+      }
   console.log(row);
   }
 }

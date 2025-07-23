9edfc62753cacf943fcabcb045d45eb964ff25f2
ying1488 <yingintech@gmail.com  git config --global user.name Ying>
2025-07-23 00:29:27 +0000

    task 3

--- javascript-warm_up/3-value_argument.js
+++ javascript-warm_up/3-value_argument.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
 const arg = process.argv.slice(2);
 
-if(!arg[0]) {
-    console.log('No argument');
+if (!arg[0]) {
+  console.log('No argument');
 } else {
-    console.log(arg[0]);
+  console.log(arg[0]);
 }

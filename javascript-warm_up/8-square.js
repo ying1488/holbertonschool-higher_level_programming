4faaa21bba5764e42eb9a33088f8447a4d86144b
ying1488 <yingintech@gmail.com  git config --global user.name Ying>
2025-07-23 00:52:43 +0000

    task 8

--- javascript-warm_up/8-square.js
+++ javascript-warm_up/8-square.js
@@ -1,15 +1,14 @@
 #!/usr/bin/node
 
-const x = parseInt(process.argv[2]);
+const num = parseInt(process.argv[2]);
 
-if (!Number.isInteger(x)) {
+if (!Number.isInteger(num)) {
   console.log('Missing size');
 } else {
-    for (let i = 0; i < x; i++) {
+    for (let i = 0; i < num; i++) {
         let row = '';
-        for (let j = 0; j < x; j++)
-        {
-            row += 'x'
+        for (let j = 0; j < num; j++) {
+            row += 'X'
         }
     console.log(row);
   }

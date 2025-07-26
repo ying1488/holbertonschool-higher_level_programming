00d8f0823e2d7de47e853a92056a1e124e83311c
ying1488 <yingintech@gmail.com  git config --global user.name Ying>
2025-07-26 08:47:31 +0000

    task 11

---
+++ javascript-warm_up/11-second_biggest.js
@@ -0,0 +1,17 @@
+#!/usr/bin/node
+
+const arg1 = process.argv.slice(2).map(Number);
+function getSecondLargest(array){
+    const n = array.length;
+    //sort array in accending order
+    array.sort((a,b)=> a - b);
+    // from second last element as the last is the largest
+    for (let i = n - 2; i >=0; i--){
+        //return the the second largest element
+        if (array[i] !== array[n - 1]){
+            return array[i];
+        }
+    }
+    return 0;
+}
+console.log(getSecondLargest(arg1));

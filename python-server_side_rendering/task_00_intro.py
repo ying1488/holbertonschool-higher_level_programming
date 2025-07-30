e905c76d40adc25224f0079bc0a8fb9e16453bfe
ying1488 <yingintech@gmail.com  git config --global user.name Ying>
2025-07-30 07:14:36 +0000

    task 0

---
+++ python-server_side_rendering/task_00_intro.py
@@ -0,0 +1,43 @@
+import sys, os, logging
+
+def generate_invitations(template_content, attendees):
+    if not isinstance(template_content, str) or not isinstance(attendees, list):
+        sys.exit()
+
+    # remove whitespacesfrom input
+    template_content = template_content.strip()
+
+    if len(template_content) == 0:
+        logging.warning("template_content is empty")
+        sys.exit()
+    
+    if len(attendees) == 0:
+        logging.warning("attendees is empty")
+        sys.exit()
+    
+    count = 1 
+    for row in attendees:
+        template_copy = template_content
+
+        for key in ["name", "event_title", "event_date", "event_location"]:
+            value = ""
+            to_replace = "{" + key + "}"
+
+            # Not just the value! if the key is missing, we should also add 'N/A'
+            if key not in row: 
+                value = "N/A"
+            else:
+                value = row[key]
+            
+            if value == "" or value is None:
+                value = "N/A"
+            
+            template_copy = template_copy.replace(to_replace, value)
+
+        f = open("output_" + str(count) + ".txt", "a")
+        f.write(template_copy)
+        f.close()
+
+    count = count + 1 
+    
+

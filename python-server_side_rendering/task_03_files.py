0af71a47c7ac3c30762f237d7f37cee5e336a695
ying1488 <yingintech@gmail.com  git config --global user.name Ying>
2025-08-01 04:26:09 +0000

    task 3

--- python-server_side_rendering/task_03_files.py
+++ python-server_side_rendering/task_03_files.py
@@ -1,10 +1,10 @@
+
 from flask import Flask, render_template, request
 import json, csv
 
 app = Flask(__name__)
 
 @app.route('/')
-
 def home():
     return render_template('index.html')
 
@@ -25,34 +25,47 @@ def items():
     # "items" can be accessed in items.html
     return render_template('items.html', items=item_list)
 
+# read json
+def read_json():
+    with open('products.json', 'r') as file:
+        data = json.load(file)
+    return data
+# read csv
+def read_csv():
+    with open('products.csv', newline='', encoding='utf-8') as file:
+        product_list = []
+        csv_file = csv.DictReader(file)
+        for row in csv_file:
+            product_list.append(row)
+    return product_list
+
+# route /products
 @app.route('/products')
 def products():
     source = request.args.get('source').lower()
     p_id = request.args.get('id')
 
     if source not in ['json', 'csv', 'sql']:
-        return render_template('prodcut_display.html', error="wrong source")
-    
+        return render_template('product_display.html', error="Wrong source")
+
     if source == 'json':
         products = read_json()
     elif source == 'csv':
         products = read_csv()
-    
+
     if p_id:
         try:
-            p_id = int(p_id)
+            p_id = int(p_id) # convert type to use in loop
             filtered_products = [product for product in products if product['id'] == p_id]
 
             if not filtered_products:
                 return render_template('product_display.html', error="Product not found")
-        
         except ValueError:
             return render_template('product_display.html', error="Invalid product id")
-        
-        else:
-            filtered_products = products
+    else:
+        filtered_products = products
 
-        return render_template('product_display.html', prodcuts=filtered_products)
+    return render_template('product_display.html', products=filtered_products)
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5000)
+    app.run(host='0.0.0.0', port=5000, debug=True)

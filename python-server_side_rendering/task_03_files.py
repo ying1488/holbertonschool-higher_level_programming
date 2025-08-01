425fa999525bb8f7d862b700be76114cca0d977d
ying1488 <yingintech@gmail.com  git config --global user.name Ying>
2025-08-01 04:21:15 +0000

    task 3

---
+++ python-server_side_rendering/task_03_files.py
@@ -0,0 +1,58 @@
+from flask import Flask, render_template, request
+import json, csv
+
+app = Flask(__name__)
+
+@app.route('/')
+
+def home():
+    return render_template('index.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
+
+@app.route('/items')
+def items():
+    with open('items.json', 'r') as file:
+        data = json.load(file)
+
+        item_list = data.get("items", [])
+    # "items" can be accessed in items.html
+    return render_template('items.html', items=item_list)
+
+@app.route('/products')
+def products():
+    source = request.args.get('source').lower()
+    p_id = request.args.get('id')
+
+    if source not in ['json', 'csv', 'sql']:
+        return render_template('prodcut_display.html', error="wrong source")
+    
+    if source == 'json':
+        products = read_json()
+    elif source == 'csv':
+        products = read_csv()
+    
+    if p_id:
+        try:
+            p_id = int(p_id)
+            filtered_products = [product for product in products if product['id'] == p_id]
+
+            if not filtered_products:
+                return render_template('product_display.html', error="Product not found")
+        
+        except ValueError:
+            return render_template('product_display.html', error="Invalid product id")
+        
+        else:
+            filtered_products = products
+
+        return render_template('product_display.html', prodcuts=filtered_products)
+
+if __name__ == '__main__':
+    app.run(debug=True, port=5000)

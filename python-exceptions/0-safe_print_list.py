8f1dd7a9cb06f11a6c8c4aab453e4814e76d9e2f
ying1488 <yingintech@gmail.com  git config --global user.name Ying>
2025-08-04 03:42:23 +0000

    task 0

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
-    num = 0
     try:
+        count = 0
         for i in range(x):
             print(my_list[i], end="")
-            num += 1
+            count += 1
+        print()
+        return count
     except IndexError:
-        pass
-    print()
-    return num
+        print()
+        return count

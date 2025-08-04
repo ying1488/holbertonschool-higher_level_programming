5bb242300eb62e7df42ec46d18bbe5ab1b36f418
ying1488 <yingintech@gmail.com  git config --global user.name Ying>
2025-08-04 03:44:05 +0000

    task 0

--- python-exceptions/4-list_division.py
+++ python-exceptions/4-list_division.py
@@ -5,11 +5,11 @@ def list_division(my_list_1, my_list_2, list_length):
         res = 0
         try:
             res = my_list_1[i] / my_list_2[i]
-        except(TypeError):
+        except (TypeError):
             print("wrong type")
-        except(ZeroDivisionError):
+        except (ZeroDivisionError):
             print("division by 0")
-        except(IndexError):
+        except (IndexError):
             print("out of range")
         finally:
             newList.append(res)

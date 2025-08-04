a0998422752479a557a2a286803400aa9b0fa869
ying1488 <yingintech@gmail.com  git config --global user.name Ying>
2025-08-04 03:53:11 +0000

    task 7

--- python-inheritance/7-base_geometry.py
+++ python-inheritance/7-base_geometry.py
@@ -1,46 +1,29 @@
 #!/usr/bin/python3
 """
-This module provides a class called BaseGeometry
+Write a class BaseGeometry (based on 6-base_geometry.py).
+Raises an Exception with the message area() is not implemented
+Raise a TypeError exception, with the message <name> must be an integer
+Raise a ValueError exception with the message <name> must be greater than 0
 """
 
 
 class BaseGeometry:
     """
-    class BaseGeometry
+    Write a class BaseGeometry (based on 6-base_geometry.py).
+    Raises an Exception with the message area() is not implemented
 
-    >>> bg = BaseGeometry()
-    >>> bg.area()
-    Traceback (most recent call last):
-        ...
-    Exception: area() is not implemented
     """
-    
     def area(self):
-        """
-        function that calculates area
 
-        Parameters:
-        self: the size
-
-        Raises:
-            Exception: Indicating that the area() method is not implemented
-        """
         raise Exception("area() is not implemented")
 
+    """
+    Raise a TypeError exception, with the message <name> must be an integer
+    Raise a ValueError exception with the message <name> must be greater than 0
+    """
     def integer_validator(self, name, value):
-        """
-        function that calculates area
 
-        Parameters:
-        self: the size
-        name: name must be string
-        value: value must be integer
-
-        Raises:
-            TypeError: If 'value' is not an integer.
-            ValueError: If 'value' is less than or equal to 0
-        """
         if type(value) is not int:
             raise TypeError(f"{name} must be an integer")
-        elif value <= 0:
+        if value <= 0:
             raise ValueError(f"{name} must be greater than 0")
